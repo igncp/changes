903a7ea66d4cf71c96946defce4954e2f02bf26a
Ignacio Carbajo <icarbajop@gmail.com>
2016-11-23 21:52:41 +0800

    Complete change in project

--- javascript/sails/src/add-hook-elasticsearch/create-demo.sh
+++ javascript/sails/src/add-hook-elasticsearch/create-demo.sh
@@ -1,1 +1,31 @@
-rm -rf demo; ../../repository/bin/sails.js new demo; cd demo; node app.js
+#
+# Use npm@2.X in the sails repo to speed installation
+# npm i -g npm@2
+#
+ES_URL=http://localhost:9200/sails
+
+rm -rf demo
+curl -XDELETE "$ES_URL"
+curl -XPUT "$ES_URL"
+curl -XPUT "$ES_URL/events/_mapping" -d '
+{
+    "events" : {
+      "properties": {
+        "at": {
+          "type" : "date",
+          "format" : "epoch_millis"
+        },
+        "event": {
+          "type": "string"
+        },
+        "data": {
+          "type": "string"
+        }
+      }
+    }
+}
+'
+echo ""
+../../repository/bin/sails.js new demo
+cd demo
+node --debug app.js
